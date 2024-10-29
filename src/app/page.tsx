77bc2e7cdb0de0a5b26ba65ac676bc1306169ab3
Yoann Fleury <yoann@bearstudio.fr>
2024-10-29 11:24:10 +0100

    feat: remove useless files for minimal reproduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import { redirect } from 'next/navigation';
-
-import { ROUTES_APP } from '@/features/app/routes';
-
 export default function Page() {
-  redirect(ROUTES_APP.root());
+  return <>Hi there</>;
 }
